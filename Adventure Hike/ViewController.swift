32a1545b6546d254962ac11b3bc40b124a24e312
Ana Laura <robertogutierrezgonzalez@macbook-pro-de-roberto.local>
2016-01-03 15:16:39 -0700

    Initial Commit

---
+++ Adventure Hike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Adventure Hike
+//
+//  Created by Roberto Gutierrez on 03/01/16.
+//  Copyright © 2016 Roberto Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
